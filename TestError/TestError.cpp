9b969d1def652d7bbf59b7158c2a30bcbc3f8408
CHEN <CHEN@DESKTOP-52BGM1C>
2017-11-03 15:49:39 +0800

    添加项目文件。

---
+++ TestError/TestError.cpp
@@ -0,0 +1,66 @@
+// TestError.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+class ExceptionClass
+{
+public:
+	ExceptionClass(const char* name = "Exception Default Class")
+	{
+		cout << "Exception Class Construct String" << endl;
+	}
+	~ExceptionClass()
+	{
+		cout << "Exception Class Destruct String" << endl;
+	}
+	void ReportError()
+	{
+		cout << "Exception Class:: This is Report Error Message" << endl;
+	}
+};
+
+class ArguClass
+{
+	char* name;
+public:
+	ArguClass(char* name = "default name")
+	{
+		cout << "Construct String::" << name << endl;
+		this->name = name;
+	}
+	~ArguClass()
+	{
+		cout << "Destruct String::" << name << endl;
+	}
+	void mythrow()
+	{
+		throw ExceptionClass();
+	}
+};
+int main()
+{
+	ArguClass e("haha");
+	try
+	{
+		e.mythrow();
+	}
+	catch (int)
+	{
+		cout << "If This is Message display screen, This is a Error!!" << endl;  //这行不会执行
+	}
+	catch (ExceptionClass pTest)
+	{
+		pTest.ReportError();
+	}
+	catch (...)
+	{
+		cout << "***************" << endl;
+	}
+	system("pause");
+    return 0;
+}
+
